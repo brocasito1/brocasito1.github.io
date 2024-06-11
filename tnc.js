0affb178ee3e843a4181c8237d34fab05a75c5d8
Luiz Verissimo <130661503+brocasito1@users.noreply.github.com>
2024-06-11 19:40:27 -0300

    Add files via upload

--- tnc.js
+++ tnc.js
@@ -1,3 +1,18 @@
+
+function moveButton() {
+    const button = document.getElementById('moveButton');
+    
+    // Gera novas coordenadas aleatórias dentro da janela
+    const maxX = window.innerWidth - button.offsetWidth;
+    const maxY = window.innerHeight - button.offsetHeight;
+    const newX = Math.floor(Math.random() * maxX);
+    const newY = Math.floor(Math.random() * maxY);
+    
+    // Define as novas coordenadas
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
+}
+
 function showMessage() {
     const message = document.getElementById('message');
     message.classList.remove('hidden');
@@ -11,4 +26,3 @@ document.addEventListener('DOMContentLoaded', () => {
     button.style.left = '0px';
     button.style.top = '0px';
 });
-
